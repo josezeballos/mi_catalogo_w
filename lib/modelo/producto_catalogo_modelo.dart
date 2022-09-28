7330e76582eb14ae1714f46dc64810862a7959fa
josezeballos <josezeballosflores@gmail.com>
2022-09-28 16:36:37 -0400

    carga imagenes al storage de firebase y guarda el ProductoCatalogo en realtime database de firebase

--- lib/modelo/producto_catalogo_modelo.dart
+++ lib/modelo/producto_catalogo_modelo.dart
@@ -3,13 +3,16 @@
 //     final productoCatalogoModelo = productoCatalogoModeloFromJson(jsonString);
 
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-ProductoCatalogoModelo productoCatalogoModeloFromJson(String str) => ProductoCatalogoModelo.fromJson(json.decode(str));
+ProductoCatalogoModelo productoCatalogoModeloFromJson(String str) =>
+    ProductoCatalogoModelo.fromJson(json.decode(str));
 
-String productoCatalogoModeloToJson(ProductoCatalogoModelo data) => json.encode(data.toJson());
+String productoCatalogoModeloToJson(ProductoCatalogoModelo data) =>
+    json.encode(data.toJson());
 
 ///clase modelo del producto
 class ProductoCatalogoModelo {
@@ -24,30 +27,39 @@ class ProductoCatalogoModelo {
   String nombre;
   double precio;
   String? descripcion;
-  final  database = FirebaseDatabase.instance;
 
-  factory ProductoCatalogoModelo.fromJson(Map<String, dynamic> json) => ProductoCatalogoModelo(
-    img: json["img"],
-    nombre: json["nombre"],
-    precio: json["precio"].toDouble(),
-    descripcion: json["descripcion"],
-  );
+  final database = FirebaseDatabase.instance;
+
+  factory ProductoCatalogoModelo.fromJson(Map<String, dynamic> json) =>
+      ProductoCatalogoModelo(
+        img: json["img"],
+        nombre: json["nombre"],
+        precio: json["precio"].toDouble(),
+        descripcion: json["descripcion"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "img": img,
-    "nombre": nombre,
-    "precio": precio,
-    "descripcion": descripcion,
-  };
+        "img": img,
+        "nombre": nombre,
+        "precio": precio,
+        "descripcion": descripcion,
+      };
 
- guardarProducto() async {
-   final storage = FirebaseStorage.instance.ref();
-// Child references can also take paths
-// spaceRef now points to "images/space.jpg
-// imagesRef still points to "images"
-   final spaceRef = storage.child("images/");
+  guardarProducto() async {
+    final storage = FirebaseStorage.instance.ref();
+    final spaceRef = storage.child("images/");
+    File file = File(img);
+    try {
+      img = await spaceRef.putFile(file).snapshot.ref.getDownloadURL();
+      await database.ref("productos").child(nombre).set(toJson());
+      print(toString());
+    } catch (error) {
+      print(error);
+    }
+  }
 
-
-   // await database.ref("productos").child(nombre).set(toJson());
- }
+  @override
+  String toString() {
+    return 'ProductoCatalogoModelo{img: $img ,\n nombre: $nombre,\n precio: $precio,\n descripcion: $descripcion}';
+  }
 }
